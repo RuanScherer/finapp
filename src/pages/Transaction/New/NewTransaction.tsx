c1236bb30a91c81096dee0f7d1019ffdf7db4de9
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2023-04-20 21:10:08 -0300

    refactor: new transaction form page

--- src/pages/Transaction/New/NewTransaction.tsx
+++ src/pages/Transaction/New/NewTransaction.tsx
@@ -1,4 +1,12 @@
-import { Button, Flex, Heading, HStack, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { Container } from "@components/Container";
 import { Input } from "@components/Form/Input";
 import { Radio } from "@components/Form/Radio/Radio";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -85,195 +93,214 @@ export function NewTransaction() {
   }
 
   return (
-    <Flex
-      as="form"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      w="full"
-      maxW="600"
-      mx="auto"
-      px="4"
-      py="6"
-      onSubmit={handleSubmit(handleSave)}
-    >
-      <Heading
-        display="flex"
-        alignItems="center"
-        alignSelf="flex-start"
-        gap="4"
-        fontSize="2xl"
-        fontWeight="semibold"
-      >
-        <RxArrowLeft cursor="pointer" onClick={handleBack} />
-        Nova transação
-      </Heading>
-
-      <SimpleGrid columns={[1, 2]} gap="4" w="full" mt="10">
-        <Input
-          label="Nome"
-          error={formState.errors.name}
-          {...register("name")}
-        />
-
-        {recurrence === TransactionRecurrence.INSTALLMENT ? (
-          <Input
-            name="amount-readonly"
-            type="number"
-            step="0.01"
-            isCurrency
-            label="Valor"
-            isDisabled
-            _disabled={{
-              opacity: 1,
-              cursor: "not-allowed",
-            }}
-            value={amount}
-          />
-        ) : (
-          <Input
-            type="number"
-            step="0.01"
-            isCurrency
-            label="Valor"
-            error={formState.errors.amount}
-            {...register("amount")}
-          />
-        )}
-
-        <Input
-          list="categories"
-          label="Categoria"
-          error={formState.errors.category}
-          {...register("category")}
-        />
-        <datalist id="categories">
-          {categorySuggestions?.map((category) => (
-            <option value={category} key={category} />
-          ))}
-        </datalist>
-
-        <Input
-          list="paymentMethods"
-          label="Forma de pagamento"
-          error={formState.errors.paymentMethod}
-          {...register("paymentMethod")}
-        />
-        <datalist id="paymentMethods">
-          {paymentMethodSuggestions?.map((paymentMethod) => (
-            <option value={paymentMethod} key={paymentMethod} />
-          ))}
-        </datalist>
-
-        <SimpleGrid columns={2} gap={4}>
-          <Radio
-            label="Recorrência"
-            defaultValue={TransactionRecurrence.UNIQUE}
-            options={[
-              { value: TransactionRecurrence.UNIQUE, label: "Único" },
-              { value: TransactionRecurrence.INSTALLMENT, label: "Parcelado" },
-              { value: TransactionRecurrence.FIXED, label: "Fixo" },
-            ]}
-            {...register("recurrence")}
-          />
-
-          <Radio
-            label="Tipo"
-            defaultValue={TransactionType.OUTCOME}
-            options={[
-              { value: TransactionType.INCOME, label: "Receita" },
-              { value: TransactionType.OUTCOME, label: "Despesa" },
-            ]}
-            {...register("type")}
-          />
-
-          {(!recurrence || recurrence === "UNICO") && (
-            <Radio
-              label="Situação"
-              defaultValue={TransactionStatus.PENDENT}
-              options={[
-                { value: TransactionStatus.PENDENT, label: "Pendente" },
-                { value: TransactionStatus.SETTLED, label: "Quitado" },
-              ]}
-              {...register("status")}
-            />
-          )}
-        </SimpleGrid>
-
-        <Input
-          type="date"
-          label="Vencimento"
-          min={currentDate}
-          defaultValue={currentDate}
-          error={formState.errors.dueDate}
-          {...register("dueDate")}
-        />
-      </SimpleGrid>
-
-      {recurrence === TransactionRecurrence.INSTALLMENT && (
-        <>
-          <Heading mt="6" fontSize="xl" fontWeight="semibold" w="full">
-            Parcelamento
-          </Heading>
-
-          <SimpleGrid columns={2} gap="4" w="full" mt="2">
-            <Input
-              type="number"
-              label="Quantidade de parcelas"
-              error={formState.errors.installmentAmount}
-              {...register("installmentAmount", {
-                onChange: (e) => {
-                  const installmentAmount = Number(e.target.value);
-                  const amount =
-                    (installmentValue ?? 0) * (installmentAmount ?? 1);
-                  if (amount) {
-                    setValue("amount", Number(amount.toFixed(2)));
-                  }
-                },
-              })}
+    <>
+      <Box bgColor="primary.500" shadow="md">
+        <Container>
+          <HStack justifyContent="center" gap={1}>
+            <RxArrowLeft
+              cursor="pointer"
+              size={18}
+              color="white"
+              strokeWidth={1}
+              onClick={handleBack}
             />
 
+            <Heading
+              fontSize={["lg", "xl", "2xl"]}
+              fontWeight="semibold"
+              color="whiteAlpha.900"
+            >
+              Nova transação
+            </Heading>
+
+            <Box visibility="hidden">
+              <RxArrowLeft />
+            </Box>
+          </HStack>
+        </Container>
+      </Box>
+
+      <Container py={[4, 8]}>
+        <Flex
+          as="form"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          w="full"
+          maxW="600"
+          mx="auto"
+          onSubmit={handleSubmit(handleSave)}
+        >
+          <SimpleGrid columns={[1, 2]} gap="4" w="full">
             <Input
-              type="number"
-              label="Valor da parcela"
-              error={formState.errors.installmentValue}
-              isCurrency
-              step="0.01"
-              {...register("installmentValue", {
-                onChange: (e) => {
-                  const installmentValue = Number(e.target.value);
-                  const amount =
-                    (installmentValue ?? 0) * (installmentAmount ?? 1);
-                  if (amount) {
-                    setValue("amount", Number(amount.toFixed(2)));
-                  }
-                },
-              })}
+              label="Nome"
+              error={formState.errors.name}
+              {...register("name")}
+            />
+
+            {recurrence === TransactionRecurrence.INSTALLMENT ? (
+              <Input
+                name="amount-readonly"
+                type="number"
+                step="0.01"
+                isCurrency
+                label="Valor"
+                isDisabled
+                _disabled={{
+                  opacity: 1,
+                  cursor: "not-allowed",
+                }}
+                value={amount}
+              />
+            ) : (
+              <Input
+                type="number"
+                step="0.01"
+                isCurrency
+                label="Valor"
+                error={formState.errors.amount}
+                {...register("amount")}
+              />
+            )}
+
+            <Input
+              list="categories"
+              label="Categoria"
+              error={formState.errors.category}
+              {...register("category")}
+            />
+            <datalist id="categories">
+              {categorySuggestions?.map((category) => (
+                <option value={category} key={category} />
+              ))}
+            </datalist>
+
+            <Input
+              list="paymentMethods"
+              label="Forma de pagamento"
+              error={formState.errors.paymentMethod}
+              {...register("paymentMethod")}
+            />
+            <datalist id="paymentMethods">
+              {paymentMethodSuggestions?.map((paymentMethod) => (
+                <option value={paymentMethod} key={paymentMethod} />
+              ))}
+            </datalist>
+
+            <SimpleGrid columns={2} gap={4}>
+              <Radio
+                label="Recorrência"
+                defaultValue={TransactionRecurrence.UNIQUE}
+                options={[
+                  { value: TransactionRecurrence.UNIQUE, label: "Único" },
+                  {
+                    value: TransactionRecurrence.INSTALLMENT,
+                    label: "Parcelado",
+                  },
+                  { value: TransactionRecurrence.FIXED, label: "Fixo" },
+                ]}
+                {...register("recurrence")}
+              />
+
+              <Radio
+                label="Tipo"
+                defaultValue={TransactionType.OUTCOME}
+                options={[
+                  { value: TransactionType.INCOME, label: "Receita" },
+                  { value: TransactionType.OUTCOME, label: "Despesa" },
+                ]}
+                {...register("type")}
+              />
+
+              {(!recurrence || recurrence === "UNICO") && (
+                <Radio
+                  label="Situação"
+                  defaultValue={TransactionStatus.PENDENT}
+                  options={[
+                    { value: TransactionStatus.PENDENT, label: "Pendente" },
+                    { value: TransactionStatus.SETTLED, label: "Quitado" },
+                  ]}
+                  {...register("status")}
+                />
+              )}
+            </SimpleGrid>
+
+            <Input
+              type="date"
+              label="Vencimento"
+              min={currentDate}
+              defaultValue={currentDate}
+              error={formState.errors.dueDate}
+              {...register("dueDate")}
             />
           </SimpleGrid>
-        </>
-      )}
 
-      <HStack justifyContent="center" gap="4" mt="10" w="full">
-        <Button
-          type="button"
-          flex="1"
-          maxW="120px"
-          fontWeight="medium"
-          onClick={handleBack}
-        >
-          Cancelar
-        </Button>
-        <Button
-          type="submit"
-          flex="1"
-          maxW="120px"
-          fontWeight="medium"
-          colorScheme="primary"
-        >
-          Salvar
-        </Button>
-      </HStack>
-    </Flex>
+          {recurrence === TransactionRecurrence.INSTALLMENT && (
+            <>
+              <Heading mt="6" fontSize="xl" fontWeight="semibold" w="full">
+                Parcelamento
+              </Heading>
+
+              <SimpleGrid columns={2} gap="4" w="full" mt="2">
+                <Input
+                  type="number"
+                  label="Quantidade de parcelas"
+                  error={formState.errors.installmentAmount}
+                  {...register("installmentAmount", {
+                    onChange: (e) => {
+                      const installmentAmount = Number(e.target.value);
+                      const amount =
+                        (installmentValue ?? 0) * (installmentAmount ?? 1);
+                      if (amount) {
+                        setValue("amount", Number(amount.toFixed(2)));
+                      }
+                    },
+                  })}
+                />
+
+                <Input
+                  type="number"
+                  label="Valor da parcela"
+                  error={formState.errors.installmentValue}
+                  isCurrency
+                  step="0.01"
+                  {...register("installmentValue", {
+                    onChange: (e) => {
+                      const installmentValue = Number(e.target.value);
+                      const amount =
+                        (installmentValue ?? 0) * (installmentAmount ?? 1);
+                      if (amount) {
+                        setValue("amount", Number(amount.toFixed(2)));
+                      }
+                    },
+                  })}
+                />
+              </SimpleGrid>
+            </>
+          )}
+
+          <HStack justifyContent="center" gap="4" mt="10" w="full">
+            <Button
+              type="button"
+              flex="1"
+              maxW="120px"
+              fontWeight="medium"
+              onClick={handleBack}
+            >
+              Cancelar
+            </Button>
+            <Button
+              type="submit"
+              flex="1"
+              maxW="120px"
+              fontWeight="medium"
+              colorScheme="primary"
+            >
+              Salvar
+            </Button>
+          </HStack>
+        </Flex>
+      </Container>
+    </>
   );
 }
